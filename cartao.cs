3cd8826cd21e91b4e75bc6edf22ee8e4e5a0baf5
HigorParnoff <55004701+HigorParnoff@users.noreply.github.com>
2019-10-02 18:37:00 -0300

    Cartao_01

--- cartao.cs
+++ cartao.cs
@@ -1,25 +1,26 @@
 using System;
 
 class Cartao {
-  private int slots;
+  private double limite;
   private double [,] faturas;
 
-  public Cartao (int qtd_slots) {
-  slots = qtd_slots;
-  faturas = new double [12, slots];
+  public Cartao (double l) {
+   limite = l;
+
   }
 
-  public void comprar (int mes, int pos, double valor){
-    faturas[mes,pos] = valor;
+  public bool comprar (double valor){
+    if(limite-valor >= 0){
+      limite = limite - valor;
+      return true;
+    }
+    else{
+      return false;
+    }
+    
   }
   
   public void ver_fatura(){
-    for(int i=0;i<12;i++){
-      for(int j=0;j<slots;j++){
-        if(faturas[i,j] != 0){
-          Console.WriteLine("Mês: {0} ---- Compra: {1} --- Valor {2}",i+1,j+1,faturas[i,j]);
-        }
-      }
-    }
+   //TODO
   }
 }
--- main.cs
+++ main.cs
@@ -2,16 +2,15 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Cartao meuCard = new Cartao(30);
-    meuCard.comprar(1,0,54.99);
-     meuCard.comprar(1,1,99);
-    meuCard.comprar(1,2,9.0);
-    meuCard.ver_fatura(); 
-
-    Cartao Card2 = new Cartao(10);
-    Card2.comprar(11, 2, 98.76);
-    Card2.ver_fatura(); 
-
-
+    double compra;
+    Cartao meuCard = new Cartao(200);
+    Console.WriteLine("VALOR:");
+    compra = double.Parse(Console.ReadLine());
+    if(meuCard.comprar(compra)==true){
+      Console.WriteLine("COMPRA EFETUADA");
+    }
+    else{
+      Console.WriteLine("COMPRA RECUSADA");
+    }
   }
 }
